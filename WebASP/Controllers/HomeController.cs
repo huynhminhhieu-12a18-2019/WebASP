171454a88dc87918f848dda2c1f3d67ff538919c
Minh-Hieu2021 <89077535+Minh-Hieu2021@users.noreply.github.com>
2021-12-30 10:53:37 +0700

    Xử lí login

--- WebASP/Controllers/HomeController.cs
+++ WebASP/Controllers/HomeController.cs
@@ -5,17 +5,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using WebASP.Data;
 using WebASP.Models;
 
 namespace WebASP.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly WebASPContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(WebASPContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +33,30 @@ namespace WebASP.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string tk, string mk)
+        {
+            int loaiTKID = (from taikhoan in _context.TaiKhoans
+                           where taikhoan.Ten == tk && taikhoan.MK == mk
+                           select taikhoan.LoaiTKId).FirstOrDefault();
+            if (loaiTKID!= 0)
+            {
+                if (loaiTKID == 1)
+                {
+                    return RedirectToAction("Index", "Admins");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                
+            }
+            return View();
         }
     }
 }
